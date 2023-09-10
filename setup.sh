17c1866367d512f3f5535b09a9ec2e8393cba1d4
suisuyy <suisuyus@gmail.com>
2023-09-10 07:23:21 +0000

    Sun, Sep 10, 2023, 3:23 PM +08:00

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 export PY_VERSIION='3.10.4'
 
-sudo apt update; sudo apt install build-essential libssl-dev zlib1g-dev \
+sudo apt update; sudo apt install -y build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev curl \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
