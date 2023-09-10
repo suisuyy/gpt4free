7de4d59a680c5fe6f96f890668387a91b56ddb75
suisuyy <suisuyus@gmail.com>
2023-09-10 10:00:07 +0000

    Sun, Sep 10, 2023, 6:00 PM +08:00

--- testing/test_async_ok.py
+++ testing/test_async_ok.py
@@ -31,7 +31,9 @@ async def run_async():
     if _provider.working and hasattr(_provider, "create_async")
   ]
   responses = [create_async(_provider) for _provider in _providers]
-  responses = await asyncio.gather(*responses)
+#  responses = await asyncio.gather(*responses)
+  responses = asyncio.gather(*responses)
+  sleep(5)
   for idx, provider in enumerate(_providers):
       print(f"{provider.__name__}:", responses[idx])
 
