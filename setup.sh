98b0497981bd0ef10c93d76512c9326b564749ff
suisuyy <suisuyus@gmail.com>
2023-09-10 07:54:39 +0000

    Sun, Sep 10, 2023, 3:54 PM +08:00

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ sudo apt update; sudo apt install -y build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev curl \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
+sudo apt install -y htop aria2
+
 curl https://pyenv.run | bash
 
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
