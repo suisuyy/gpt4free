3a32b2aca36f25150b04992c8fffdd3cf91a6f6d
suisuyy <suisuyus@gmail.com>
2023-09-10 06:23:50 +0000

    Sun, Sep 10, 2023, 2:23 PM +08:00

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+export PY_VERSIION='3.10.4'
+
 sudo apt update; sudo apt install build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev curl \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
@@ -10,8 +12,9 @@ echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
 
 . ~/.bash_profile
 
-pyenv install 3.10.4
+pyenv install $PY
 
+pyenv global 3.10.4
 
 
 
